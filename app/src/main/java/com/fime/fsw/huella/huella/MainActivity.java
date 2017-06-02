5012bb97b06ad8cdfe1ee2643adb813209e39a9b
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-06-02 15:41:31 -0500

    Initial commit, layers design

---
+++ app/src/main/java/com/fime/fsw/huella/huella/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fime.fsw.huella.huella;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
