b3db39aa353b6f72f04e3eabf1f60ac5bff080c5
Enrique <enriquesaalas@gmail.com>
2017-06-28 00:14:10 -0500

    Add Aplicacion de prueba para el consumo del web service

---
+++ HuellaApiTest/app/src/main/java/com/ensardz/huellaapitest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ensardz.huellaapitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // https://young-escarpment-48238.herokuapp.com/routes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
