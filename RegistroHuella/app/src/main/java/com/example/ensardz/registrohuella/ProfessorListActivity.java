107262478439dc5b42643096f67bd219740415ee
ensardz <enriquesaalas@gmail.com>
2017-08-04 17:49:05 -0500

    Add algo a list activity

--- RegistroHuella/app/src/main/java/com/example/ensardz/registrohuella/ProfessorListActivity.java
+++ RegistroHuella/app/src/main/java/com/example/ensardz/registrohuella/ProfessorListActivity.java
@@ -9,5 +9,7 @@ public class ProfessorListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor_list);
+
+
     }
 }
