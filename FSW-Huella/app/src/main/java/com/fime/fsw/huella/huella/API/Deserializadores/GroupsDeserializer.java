037495595ddb06029829e3274d1612f92a489f1f
ensardz <enriquesaalas@gmail.com>
2017-09-09 22:14:08 -0500

    Add apicalls para los nuevos endpoints y extraccion de datos de los archivos JSON

---
+++ FSW-Huella/app/src/main/java/com/fime/fsw/huella/huella/API/Deserializadores/GroupsDeserializer.java
@@ -0,0 +1,10 @@
+package com.fime.fsw.huella.huella.API.Deserializadores;
+
+import com.google.gson.JsonDeserializer;
+
+/**
+ * Created by quiqu on 09/09/2017.
+ */
+
+public class GroupsDeserializer implements JsonDeserializer {
+}
---
+++ FSW-Huella/app/src/main/java/com/fime/fsw/huella/huella/API/Deserializadores/PrefectosDeserializer.java
@@ -0,0 +1,8 @@
+package com.fime.fsw.huella.huella.API.Deserializadores;
+
+/**
+ * Created by quiqu on 09/09/2017.
+ */
+
+public class PrefectosDeserializer {
+}
